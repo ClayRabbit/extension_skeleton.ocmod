c0c3c34420d817baec8db2752fa4865faa64b77c
ClayRabbit <garlic@mail.ru>
2020-10-14 00:21:08 +0500

    some updates

--- upload/admin/language/en-gb/extension/module/extension_skeleton.php
+++ upload/admin/language/en-gb/extension/module/extension_skeleton.php
@@ -7,9 +7,9 @@ $_['text_success']     = 'Success!';
 $_['text_edit']        = 'Edit';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_setting']    = 'Setting';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry']['status']     = 'Status';
+$_['entry']['setting']    = 'Setting';
+$_['entry']['sort_order'] = 'Sort Order';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify this!';
